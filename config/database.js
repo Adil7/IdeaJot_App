72be344319339d28e8d55b0c8096502bbe7f92d4
Adil7 <Adil@Adils-MacBook-Pro.local>
2018-02-23 18:14:37 -0500

    updated db config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // in production or local host set up for mongodb
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds147118.mlab.com:47118/ideajot-prod'}
+  module.exports = {mongoURI: 'mongodb://adil:54321@ds147118.mlab.com:47118/ideajot-prod'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
